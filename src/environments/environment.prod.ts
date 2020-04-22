22bb38a35ce16250a967174ddf8ae4de56713630
Gabriel Pérez <sergedxd@gmail.com>
2020-04-22 15:48:32 -0500

    Cambio de idioma e imagen en firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyAFDEWjjwEips_9vWju0_PPcy5185f5ZQc",
+    authDomain: "angular-crud-firebase-5fe2b.firebaseapp.com",
+    databaseURL: "https://angular-crud-firebase-5fe2b.firebaseio.com",
+    projectId: "angular-crud-firebase-5fe2b",
+    storageBucket: "angular-crud-firebase-5fe2b.appspot.com",
+    messagingSenderId: "850522392267",
+    appId: "1:850522392267:web:ed47480746a85f5491bd4e",
+    measurementId: "G-CJBE3BTYC6"
+  }
 };
